0f2e157b215854e96f417d95b41f968e5b7f4f52
Gregory Chen <gregorychen3@gmail.com>
2021-11-28 22:06:43 -0500

    mui 5

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,34 +1,13 @@
-import { Typography } from "@material-ui/core";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import ArrowRightIcon from "@material-ui/icons/ArrowRight";
 import { LabelDivider } from "mui-label-divider";
-import React, { useState } from "react";
 
 export default function App() {
-  const [open, setIsOpen] = useState(false);
-  const toggleOpen = () => setIsOpen((prev) => !prev);
-
   return (
     <>
-      <LabelDivider>SIMPLE</LabelDivider>
-      <LabelDivider onClick={() => alert("clicked")}>CLICKABLE</LabelDivider>
-      <CollapsibleLabelDivider open={open} onClick={toggleOpen} />
-      {open && "More Content"}
+      <LabelDivider label="SIMPLE" />
+      <LabelDivider label="CLICKABLE" onClick={() => alert("clicked")} />
+      <LabelDivider label="COLLAPSIBLE" collapsible>
+        content
+      </LabelDivider>
     </>
   );
 }
-
-interface CollapsibleLabelDividerProps {
-  open: boolean;
-  onClick: () => void;
-}
-const CollapsibleLabelDivider = ({ open, onClick }: CollapsibleLabelDividerProps) => {
-  return (
-    <LabelDivider onClick={onClick}>
-      {open ? <ArrowDropDownIcon color="disabled" /> : <ArrowRightIcon color="disabled" />}
-      <Typography variant="subtitle2" color="textSecondary" align="center">
-        EXPANDABLE
-      </Typography>
-    </LabelDivider>
-  );
-};
